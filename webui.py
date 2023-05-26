69d6b6ccf432e8b992fb9108d1d3b5478bf50390
kayoxu <371714752@qq.com>
2023-05-26 16:03:28 +0800

    share

--- webui.py
+++ webui.py
@@ -1,17 +1,23 @@
 import gradio as gr
 
+import infer
+
+
+def load_model(input):
+    infer.main(load_8bit=True, base_model='decapoda-research/llama-7b-hf', lora_weights='./lora-llama-med',
+               use_lora=True, prompt_template='med_template')
+
+def submint_disease(input):
+    return "Hello " + input + "!"
+
+
 app = gr.Blocks()
 
+diagnosis = gr.TextArea()
+
 with app:
-    gr.Markdown(value="""
-        ### sovits4.0 webui 推理&训练 v2.0.0
+    gr.Button(value="加载模型", fn=load_model)
 
-        修改自原项目及bilibili@麦哲云
+    gr.Interface(fn=submint_disease, inputs="text_area", outputs=diagnosis)
 
-        仅供个人娱乐和非商业用途，禁止用于血腥、暴力、性相关、政治相关内容
-
-        作者：bilibili@羽毛布団
-
-        """)
-
-app.launch(server_name="127.0.0.1", inbrowser=True, share=True)
+app.launch(server_name="127.0.0.1", inbrowser=True, share=False)
