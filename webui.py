4547686e73926b71aaf8d28b22691d210e3348bf
kayoxu <371714752@qq.com>
2023-05-26 15:17:25 +0800

    share

--- webui.py
+++ webui.py
@@ -2,4 +2,16 @@ import gradio as gr
 
 app = gr.Blocks()
 
+with app:
+    gr.Markdown(value="""
+        ### sovits4.0 webui 推理&训练 v2.0.0
+
+        修改自原项目及bilibili@麦哲云
+
+        仅供个人娱乐和非商业用途，禁止用于血腥、暴力、性相关、政治相关内容
+
+        作者：bilibili@羽毛布団
+
+        """)
+
 app.launch(server_name="127.0.0.1", inbrowser=True, share=True)
